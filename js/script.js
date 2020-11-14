e923a93cc76af17d6c4abe3e351ba6f6f199d809
rafliyulistiawan <rafliyulistiawan@gmail.com>
2020-11-15 04:56:36 +0700

    perbaikan sedikit tampilan

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-      console.log("tesete");
-    } else {
-      x.style.display = "flex";
-      console.log("tetetets");
-    }
-}
+var x = document.querySelector('span.nav-bar');
+var menu = document.querySelector('.menu')
+
+x.addEventListener('click', function() {
+  if (menu.style.maxHeight == "0px") {
+    console.log("tes123");
+    menu.style.maxHeight = '100%';
+  } else {
+    console.log("ga jalan");
+    menu.style.maxHeight = '0px';
+  }
+})
